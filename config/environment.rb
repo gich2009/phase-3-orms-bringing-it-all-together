92ac373b11ef6d2650738880ce74f4e8bd0e724f
gich2009 <waneneamos@gmail.com>
2023-06-11 13:37:23 +0300

    Complete assignment

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,6 @@
 require 'bundler'
 Bundler.require
 
-DB = { conn: SQLite3::Database.new("db/dogs.db") }
-
 require_relative '../lib/dog'
 
-# DB = { conn: SQLite3::Database.new("db/dogs.db") }
+DB = { conn: SQLite3::Database.new("db/dogs.db") }
